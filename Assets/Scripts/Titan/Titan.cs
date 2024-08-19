e54359ece959a7a2ba9361e860900fde5f41f661
Dan <vdaniel2001@gmail.com>
2024-08-20 00:32:46 +0300

    titan scale anim speed

--- Assets/Scripts/Titan/Titan.cs
+++ Assets/Scripts/Titan/Titan.cs
@@ -10,6 +10,7 @@ namespace Titan
     public class Titan : MonoBehaviour
     {
         public Animator Animator;
+        public GameObject Hand;
         public TitanAgroRange AgroRange;
         public TitanMouth Mouth;
         public GameObject Head;
@@ -40,6 +41,8 @@ namespace Titan
 
         private void Awake()
         {
+            Animator.SetFloat("speed_multiplier",1/Mathf.Sqrt(transform.localScale.y-4));
+            Hand.GetComponent<Animator>().SetFloat("speed_multiplier", 1 /( Mathf.Sqrt((transform.localScale.y-4)*2)));
             AgroRange.OnAgro += OnAgro;
             
             foreach (var weakSpot in WeakSpots)
