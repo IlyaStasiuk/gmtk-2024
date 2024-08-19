a81c5b7fe080d3337efa34814e902a4377711b1d
DESKTOP-3FKA7L5\user <vladboroday@gmail.com>
2024-08-19 10:57:02 -0700

    updated scene restarter, player can no longer use hooks after death

--- Assets/Scripts/SceneRestarter.cs
+++ Assets/Scripts/SceneRestarter.cs
@@ -4,14 +4,34 @@ using UnityEngine.SceneManagement;
 
 public class SceneRestarter : MonoBehaviour
 {
-        [SerializeField] private Transform target;
-        [SerializeField] private float altitude;
+    [SerializeField] private Transform target;
+    [SerializeField] private float deathAltitude;
+    [SerializeField] private float restartAfterTimeInDeathZone = 1.0f;
 
-        private void LateUpdate()
+    private bool isInDeathZone = false;
+    private float durationInDeathZone = 0.0f;
+
+    public static SceneRestarter instance;
+
+    public bool IsInDeathZone => isInDeathZone;
+    public float DurationInDeathZone => durationInDeathZone;
+    
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void LateUpdate()
+    {
+        if (target.transform.position.y > deathAltitude)
+            return;
+
+        isInDeathZone = true;
+        durationInDeathZone += Time.deltaTime;
+
+        if (durationInDeathZone > restartAfterTimeInDeathZone)
         {
-                if(target.transform.position.y > altitude)
-                        return;
-
-                SceneManager.LoadScene(SceneManager.GetActiveScene().name, LoadSceneMode.Single);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name, LoadSceneMode.Single);
         }
+    }
 }
